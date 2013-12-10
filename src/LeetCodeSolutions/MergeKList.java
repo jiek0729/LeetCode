e6fb52adad9bfbc836a5f31be395f42cb037ce4a
jiek0729 <kangjie112@gmail.com>
2013-12-09 21:33:11 -0600

    Test merge tool

--- src/LeetCodeSolutions/MergeKList.java
+++ src/LeetCodeSolutions/MergeKList.java
@@ -4,10 +4,10 @@ import java.util.*;
 
 public class MergeKList {
 	public ListNode mergeKLists(ArrayList<ListNode> lists) {
-		System.out.println("start");
-		
+		// Create dummy head list node.
         ListNode head = new ListNode(0);
         
+        // Instantiate priority queue as min heap.
         PriorityQueue<ListNode> queue = new PriorityQueue<ListNode>(lists.size() + 1,
         new Comparator<ListNode>(){
             public int compare(ListNode node1, ListNode node2){
@@ -15,18 +15,16 @@ public class MergeKList {
             }
         });
         
-        System.out.println("priority queue initialized");
-        
+        // Initializes min heap with lists.
         for(ListNode node : lists){
             queue.add(node);
         }
-        
-        System.out.println("Priority queue filled");
         
         ListNode cur = head;
         
         ListNode node = null;
         
+        // Merge.
         while((node = queue.poll()) != null){
             
             cur.next = node;
