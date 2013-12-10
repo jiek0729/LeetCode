777429ab26d7a59817e7c2246e893c2eadca8be0
jiek0729 <kangjie112@gmail.com>
2013-12-09 21:20:31 -0600

    Test Commit
    Merge k sorted list.

---
+++ src/LeetCodeSolutions/Test.java
@@ -0,0 +1,49 @@
+package LeetCodeSolutions;
+
+import java.util.*;
+
+public class Test {
+	public ListNode mergeKLists(ArrayList<ListNode> lists) {
+		System.out.println("start");
+		
+        ListNode head = new ListNode(0);
+        
+        PriorityQueue<ListNode> queue = new PriorityQueue<ListNode>(lists.size() + 1,
+        new Comparator<ListNode>(){
+            public int compare(ListNode node1, ListNode node2){
+                return node1.val - node2.val;
+            }
+        });
+        
+        System.out.println("priority queue initialized");
+        
+        for(ListNode node : lists){
+            queue.add(node);
+        }
+        
+        System.out.println("Priority queue filled");
+        
+        ListNode cur = head;
+        
+        ListNode node = null;
+        
+        while((node = queue.poll()) != null){
+            
+            cur.next = node;
+            
+            if(node.next != null){
+                queue.offer(node.next);
+            }
+            
+            cur = cur.next;
+        }
+        
+        return head.next;
+    }
+	
+	public static void main(String[] args){
+		Test test = new Test();
+		
+		test.mergeKLists(new ArrayList<ListNode>());
+	}
+}
